00fa1de775b692be08aab0d4c563cea1ee15f1b9
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-14 00:00:20 +0200

    returns a boolean if all the elements in the array exist within the set.

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,19 @@
+/**
+ * hasValuesFromArray - returns a boolean if all the elements in the array exist within the set.
+ * @param {*} set - a set to compare values in array
+ * @param {*} array - an array to check if all values are in set
+ * @returns - returns a boolean if all the elements in the array exist within the set.
+ */
+export default function hasValuesFromArray(set, array) {
+  let count = 0;
+  array.map((item) => {
+    if (set.has(item)) {
+      count += 1;
+    }
+    return count;
+  });
+  if (array.length === count) {
+    return true;
+  }
+  return false
+}
