fa9f773e25a5ffa01a99d410f775693df3c9e0d1
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-13 23:21:46 +0200

    returns a Set from an array.

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * setFromArray - returns a Set from an array.
+ * @param {*} array - an array to produce a set from.
+ * @returns - returns a Set from an array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
