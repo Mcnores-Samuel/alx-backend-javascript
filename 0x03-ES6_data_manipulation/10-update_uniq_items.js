3e4be2303655ec834d6dd456a26945a9bf208791
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-14 02:48:19 +0200

    returns an updated map for all items with initial quantity at 1.

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -4,11 +4,11 @@
  * @returns - returns an updated map for all items with initial quantity at 1.
  */
 export default function updateUniqueItems(mapObj) {
-  if (!mapObj || !(mapObj instanceof Map)) return 'Cannot process';
-
-  const updatedMap = new Map(mapObj);
-  updatedMap.forEach((value, key) => {
-    if (value === 1) updatedMap.set(key, 100);
+  if (!mapObj || !(mapObj instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  mapObj.forEach((value, key) => {
+    if (value === 1) mapObj.set(key, 100);
   });
-  return updatedMap;
+  return mapObj;
 }
