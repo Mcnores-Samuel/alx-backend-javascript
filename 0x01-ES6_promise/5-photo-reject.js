82c48688c7bef2d49e28db6db5088b69c55a6f12
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-31 15:23:07 +0200

    return a Promise rejecting with an Error and the string  cannot be processed

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,7 @@
+export default function uploadPhoto(filename) {
+  return new Promise((resolve, reject) => {
+    if (filename) {
+      reject(new Error(`${filename} cannot be processed`));
+    }
+  });
+}
