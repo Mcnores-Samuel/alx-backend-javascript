c9f201164ccefd587808fa6550067cb57128b567
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-31 13:53:47 +0200

    promises, use the prototype below to collectively resolve all promises and log body firstName lastName to the console.
    function handleProfileSignup()
    In the event of an error, log Signup system offline to the console

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils.js'
 
 export default function handleProfileSignup() {
-  return Promise.all(uploadPhoto(), createUser())
+  return Promise.all([uploadPhoto(), createUser()])
     .then((response) => console.log(`${response[0].body} \
     ${response[1].firstName} ${response[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
