d42a5ce22c392b7df3e70e4dbcba750c6c89b8a4
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-31 11:52:57 +0200

    returns a promise resolved or rejected depending on the boolean value passed as a parameter

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,8 +4,11 @@
  */
 export default function getFullResponseFromAPI(success) {
   const promise = new Promise((resolve, reject) => {
-    if (success) resolve({ status: 200, body: 'success' });
-    reject(new Error('The fake API is not working currently'));
+    if (success) {
+      resolve({ status: 200, body: 'success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
   });
   return promise;
 }
