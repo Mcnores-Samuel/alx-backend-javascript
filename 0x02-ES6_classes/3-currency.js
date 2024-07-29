162b77496501a74c67570d69475f29470028bfa6
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-07-30 01:08:32 +0200

    updating task currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,21 +1,4 @@
-/**
- * Currency - class
- * @param {string} code - The currency code.
- * @param {string} name - The currency name.
- * Implement a class named Currency that has:
- *  - A constructor that takes two parameters: code and name.
- * - A getter for code that returns the currency code.
- * - A setter for code that sets the currency code.
- * - A getter for name that returns the currency name.
- * - A setter for name that sets the currency name.
- * - A method named displayFullCurrency that returns the currency code and name.
- */
 export default class Currency {
-  /**
-   * constructor - Creates a new Currency.
-   * @param {*} code - The currency code.
-   * @param {*} name - The currency name.
-   */
   constructor(code, name) {
     this.code = code;
     this.name = name;
@@ -36,9 +19,12 @@ export default class Currency {
   set name(value) {
     this._name = value;
   }
-  
-  displayFullCurrency() {
-    return `${this._code} (${this._name})`;
-  }
 
+  /**
+   * Creates the full string representation of this Currency.
+   * @returns {String}
+   */
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
